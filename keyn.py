0ebe9343b443ba16a2d0f5d52667594dfe757590
steven <steven@keyn.io>
2019-07-23 17:22:03 +0200

    Added csv export and print_accounts

--- keyn.py
+++ keyn.py
@@ -1,7 +1,10 @@
 import argparse
 import crypto
 import api
+import json
+import csv
 from password_generator import PasswordGenerator
+
 
 def generate():
     seed = crypto.generate_seed()
@@ -9,22 +12,51 @@ def generate():
     print(" ".join(mnemonic))
 
 
+def print_accounts(mnemonic):
+    accounts = recover(mnemonic)
+    for account in accounts:
+        print("-------------------------")
+        print("Username:\t%s" % account["username"])
+        print("Password:\t%s" % account["password"])
+        print("Site:\t\t%s" % account["site_name"])
+        print("URL:\t\t%s" % account["url"])
+
+    print("-------------------------")
+
 def recover(mnemonic):
     seed = crypto.recover(mnemonic)
     password_key, signing_keypair, decryption_key = crypto.derive_keys_from_seed(seed)
-    # accounts = api.get_backup_data(signing_keypair)
-    # for id, account in accounts.items():
-    #     print(crypto.decrypt(account, decryption_key))
-    generator = PasswordGenerator("test", "test", seed, None)
-    generator.generate_password_candidate(0, 22, None)
+    accounts = api.get_backup_data(signing_keypair)
+    accounts_export = []
+    for id, account in accounts.items():
+        decrypted_account_byte = crypto.decrypt(account, decryption_key)
+        decrypted_account_string = decrypted_account_byte.decode('utf-8').replace("'", '"')
+        decrypted_account = json.loads(decrypted_account_string)
+
+        username = decrypted_account["username"]
+        site = decrypted_account["sites"][0]
+
+        generator = PasswordGenerator(username, site["id"], seed, None)
+        password, index = generator.generate(decrypted_account["passwordIndex"], decrypted_account["passwordOffset"])
+
+        accounts_export.append({"username": username, "password": password, "url": site["url"], "site_name": site["name"]})
+
+    return accounts_export
+
 
 def import_csv(file, mnemonic):
     print("lets import")
 
 
-def export_csv(file, mnemonic):
-    print(file)
-    print(mnemonic)
+def export_csv(mnemonic):
+    accounts = recover(mnemonic)
+
+    with open('account_data.csv', mode='w') as account_data:
+        account_data_writer = csv.writer(account_data, delimiter=',')
+        account_data_writer.writerow(['URL', 'Username', 'Password', 'Site'])
+        for account in accounts:
+            account_data_writer.writerow([account["url"], account["username"], account["password"], account["site_name"]])
+
 
 
 if __name__ == '__main__':
@@ -34,6 +66,8 @@ if __name__ == '__main__':
     parser.add_argument("-m", "--mnemonic", nargs=12,
                         help="The 12-word mnemonic, e.g. -m "
                              "word1 word2 word3 word4 word5 word6 word7 word8 word9 word10 word11 word12")
+    parser.add_argument("-f", "--format", nargs=1,
+                        help="The output format"),
     parser.add_argument("-o", "--output", nargs=1,
                         help="The path to where the csv file should be written. Prints to stdout if not provided")
     parser.add_argument("-i", "--input", nargs=1,
@@ -41,11 +75,20 @@ if __name__ == '__main__':
                              "Expects input on stdin if not provided")
 
     args = parser.parse_args()
+    args.format = "csv"
+
     if args.action == "generate":
         generate()
     elif args.action == "recover":
         if args.mnemonic:
-            recover(args.mnemonic)
+            if args.format == "csv":
+                export_csv(args.mnemonic)
+            elif args.format == "json":
+                print("export_json(args.mnemonic)")
+            elif args.format == "kdbx":
+                print("export_kdbx(args.mnemonic)")
+            else:
+                print_accounts(args.mnemonic)
         else:
             print("The mnemonic should be provided with -m or --mnemonic. "
                   "E.g. keyn recover -m word1 word2 word3 word4 word5 word6 word7 word8 word9 word10 word11 word12")
--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,6 @@
 import crypto
 import math
-from password_validator import MAXIMAL_CHARACTER_SET
+import password_validator
 
 
 class PasswordGenerator:
@@ -11,27 +11,51 @@ class PasswordGenerator:
         self.seed = seed
         self.ppd = ppd
 
-    def generate(self):
-        return "todo"
+    def generate(self, password_index, offset):
+        length = self.length(offset is not None)
+        if length < password_validator.MIN_PASSWORD_LENGTH_BOUND:
+            raise ValueError("Password too short")
+
+        index = password_index
+        password = self.generate_password_candidate(index, length, offset)
+
+        #if offset is None add ppd later
+
+        return password, index
 
     def calculate_offset(self):
         return "todo"
 
-    def length(self):
-        return 5
+    def length(self, is_custom_password):
+        length = password_validator.MAX_PASSWORD_LENGTH_BOUND if is_custom_password else password_validator.FALLBACK_PASSWORD_LENGTH
+        chars = password_validator.MAXIMAL_CHARACTER_SET if is_custom_password else password_validator.OPTIMAL_CHARACTER_SET
+        #todo ppd
+
+        return length
 
     def generate_password_candidate(self, index, length, offset):
-        chars = MAXIMAL_CHARACTER_SET if offset is not None else self.characters()
-        key = self.generate_key(index)
-        bit_length = length * math.ceil(math.log2(len(chars))) + (128 + length - (128 % length))
-        byte_length = int(self.round_up(bit_length, base=length*8) / 8)
+        chars = password_validator.MAXIMAL_CHARACTER_SET if offset is not None else self.characters() #self.characters is chosen
+        key = self.generate_key(index) #selects what pw to generate to from thelist (if there is any)
+        #print(base64.b64encode(key))
+        #print(len(chars)) #length of the usable charsarray
+        #print(length) #length of the pw
+        bit_length = length * math.ceil(math.log2(len(chars))) + (128 + length - (128 % length)) #ammount of bits in the pw
+        byte_length = int(self.round_up(bit_length, base=length*8) / 8) #ammount of bytes in a pw
         key_data = crypto.deterministic_random_bytes(key, byte_length)
+        #print(key_data)
+        #print(base64.b64encode(key_data))
         modulus = len(chars) if offset is not None else len(chars) + 1
         offset = offset if offset is not None else []
+        bytesPerChar = int(byte_length / length)
+        password = ""
+        #print(offset)
+        #for each char in pw
         for i in range(0, length):
-            print(i)
-            print(i + (byte_length / length))
-            print(key_data[i:int(i + (byte_length / length))])
+            index = (int.from_bytes(key_data[i:i + bytesPerChar], byteorder="little") + offset[i]) % modulus
+            if index < len(chars):
+                password += chars[index]
+
+        return password
 
 
     # private func generatePasswordCandidate(index passwordIndex: Int, length: Int, offset: [Int]?) throws -> String {
@@ -43,18 +67,25 @@ class PasswordGenerator:
     #     let modulus = offset == nil ? chars.count : chars.count + 1
     #     let offset = offset ?? Array<Int>(repeatElement(0, count: length))
     #
+    #
+    #
+    #
     #     return (0..<length).reduce("") { (pw, index) -> String in
     #         let charIndex = (keyData[index..<index + (byteLength / length)].reduce(0) { ($0 << 8 + Int($1)).mod(modulus) } + offset[index]).mod(modulus)
     #         return charIndex == chars.count ? pw : pw + String(chars[charIndex])
     #     }
     # }
+    #
+    #
+    #
+
 
     def round_up(self, x, base):
         return base * round(x / base)
 
     @staticmethod
     def characters():
-        return "blablalasdlksajdlasjd"
+        return "blablalasdlksajdlasjdenhierkomtnogmeerbij"
 
     def generate_key(self, index):
         return crypto.password_key(self.seed, self.site_id, index, self.username)
