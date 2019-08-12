7a94f1e4f17eb60f111eb153277f0a4812f552f3
steven <steven@keyn.io>
2019-08-12 14:28:09 +0200

    Added the edit & delete functionality

--- keyn/main.py
+++ keyn/main.py
@@ -40,7 +40,7 @@ def export_accounts(mnemonic, format, path):
     encrypted_accounts_data = api.get_backup_data(signing_keypair)
 
     if not encrypted_accounts_data.items():
-        click.echo("This seed does not exist.")
+        click.echo("This seed does not exist or does not have any accounts linked to it.")
         sys.exit(1)
 
     accounts = decrypt_accounts(encrypted_accounts_data.items(), password_key, decryption_key)
@@ -122,40 +122,91 @@ def delete_accounts(mnemonic):
     api.delete_seed(signing_keypair)
     click.echo("The seed was successfully deleted.")
 
+
 @main.group()
-def accounts():
+def account():
     pass
 
 
-@accounts.command(name='edit')
-@click.option('-m', '--mnemonic', nargs=12, help='The 12-word mnemonic', prompt="Please enter the mnemonic: ",
-              hide_input=True)
-@click.option('-id', help='The id of the account you want to edit / delete.')
+@account.command(name='edit')
+@click.option('-m', '--mnemonic', nargs=12, help='The 12-word mnemonic')
+@click.option('-id', help='The id of the account you want to edit.')
 def edit_account(mnemonic, id):
-    print("account.edit_account function called")
-    if not id:
+    if mnemonic:
         seed = crypto.recover(mnemonic)
-        account = pick_account(seed)
-    print(account)
+    else:
+        seed = crypto.recover(obtain_mnemonic())
 
-
-@accounts.command(name='delete')
-def delete_account():
-    print("account.delete_account function called")
-    #
-
-
-def pick_account(seed):
     password_key, signing_keypair, decryption_key = crypto.derive_keys_from_seed(seed)
     encrypted_accounts_data = api.get_backup_data(signing_keypair)
-
     if not encrypted_accounts_data.items():
         click.echo("This seed does not exist. Exiting..")
         sys.exit(1)
 
     accounts = decrypt_accounts(encrypted_accounts_data.items(), password_key, decryption_key)
+    if not id:
+        account = pick_account(accounts)
+    else:
+        account = next(x for x in accounts if x["id"] == id)
 
-    click.echo("Which account do you want to edit?")
+    new_username = click.prompt('Enter a new username or press enter to skip (current: %s)' % account["username"],
+                                default="", show_default=False)
+    if new_username and click.confirm("Are you sure you want to update the username to \"%s\"?" % new_username):
+        account["username"] = new_username
+        click.echo("Username updated!")
+
+    new_password = click.prompt('Enter a new password or press enter to skip', confirmation_prompt=True, default="",
+                                show_default=False, hide_input=True)
+    if new_password:
+        account["index"] += 1
+        account["password"] = new_password
+        click.echo("Password updated!")
+
+    new_site_name = click.prompt('Enter a new site name or press enter to skip (current: %s)' % account["site_name"],
+                                 default="", show_default=False)
+    if new_site_name and click.confirm("Are you sure you want to update the username to \"%s\"?" % new_site_name):
+        account["site_name"] = new_site_name
+        click.echo("Site name updated!")
+
+    new_url = click.prompt('Enter a new URL or press enter to skip (current: %s)' % account["url"], default="",
+                           show_default=False)
+    if new_url and click.confirm("Are you sure you want to update the username to \"%s\"?" % new_url):
+        account["url"] = new_url
+        click.echo("URL updated!")
+
+    upload_account_data(account["url"], account["username"], account["password"], account["site_name"],
+                        password_key, signing_keypair, decryption_key, account["id"], account["index"])
+
+
+@account.command(name='delete')
+@click.option('-m', '--mnemonic', nargs=12, help='The 12-word mnemonic')
+@click.option('-id', help='The id of the account you want to edit / delete.')
+def delete_account(mnemonic, id):
+    if mnemonic:
+        seed = crypto.recover(mnemonic)
+    else:
+        seed = crypto.recover(obtain_mnemonic())
+
+    password_key, signing_keypair, decryption_key = crypto.derive_keys_from_seed(seed)
+    encrypted_accounts_data = api.get_backup_data(signing_keypair)
+    if not encrypted_accounts_data.items():
+        click.echo("This seed does not exist. Exiting..")
+        sys.exit(1)
+
+    accounts = decrypt_accounts(encrypted_accounts_data.items(), password_key, decryption_key)
+    if not id:
+        account = pick_account(accounts)
+    else:
+        account = next(x for x in accounts if x["id"] == id)
+
+    click.echo("Deleting account...")
+    _, signing_keypair, _ = crypto.derive_keys_from_seed(seed)
+    api.delete_account(account["id"], signing_keypair)
+    click.echo("The account was successfully deleted.")
+
+
+def pick_account(accounts):
+    click.echo("Which account do you want to edit / delete?")
     for i, account in enumerate(accounts, start=1):
         click.echo("[%d]: %s" % (i, account["site_name"]))
     choices = list(map(lambda x: "%d" % x, range(1, len(accounts) + 1)))
@@ -188,18 +239,20 @@ def decrypt_accounts(encrypted_accounts, password_key, decryption_key):
         generator = PasswordGenerator(username, site["id"], password_key, ppd)
         password, index = generator.generate(decrypted_account["passwordIndex"], offset)
         accounts.append({"id": id, "username": username, "password": password, "url": site["url"],
-                         "site_name": site["name"]})
+                         "site_name": site["name"], "index": index})
 
     return accounts
 
 
-def upload_account_data(url, username, password, site_name, password_key, signing_keypair, encryption_key):
+def upload_account_data(url, username, password, site_name, password_key, signing_keypair, encryption_key,
+                        account_id = None, password_index = 0):
     site_id, secondary_site_id = crypto.get_site_ids(url)
     site_id = site_id.decode("utf-8")
     ppd = api.get_ppd(site_id)
-    account_id = crypto.generic_hash_string(("%s_%s" % (site_id, username)))
+    if account_id is None:
+        account_id = crypto.generic_hash_string(("%s_%s" % (site_id, username)))
     generator = PasswordGenerator(username, site_id, password_key, ppd)
-    offset = generator.calculate_offset(0, password)
+    offset = generator.calculate_offset(password_index, password)
     account = {
         'id': account_id,
         'sites': [{
@@ -222,7 +275,7 @@ def obtain_mnemonic():
     try:
         words = mnemonic.split(" ")
         if len(words) != 12:
-            raise Exception("the mnemonic should consist of 12 words")
+            raise Exception("The mnemonic should consist of 12 words")
         return words
     except Exception as exception:
         print("\nError: %s\n" % exception.args)
